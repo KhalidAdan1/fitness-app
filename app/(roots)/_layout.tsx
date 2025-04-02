36d19c1dd6c01b3c2538eade8ff715be1403e7a5
Khalid <khalid13546@gmail.com>
2025-04-02 22:47:15 +0100

    front end

--- app/(roots)/_layout.tsx
+++ app/(roots)/_layout.tsx
@@ -3,7 +3,6 @@ import { Stack } from 'expo-router';
 export default function RootsLayout() {
   return (
     <Stack>
-      <Stack.Screen name="settings" options={{ headerShown: false }} />
       <Stack.Screen name="profile" options={{ headerShown: false }} />
       <Stack.Screen name="help" options={{ headerShown: false }} />
       <Stack.Screen name="unit-converter" options={{ headerShown: false }} />
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,5 +1,20 @@
-import { Redirect } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { Redirect, router } from 'expo-router';
+import { Pressable, SafeAreaView , Text} from 'react-native';
 
 export default function NotFound() {
-  return <Redirect href="/(auth)/welcome" />;
+  return (
+    <SafeAreaView className='flex items-center justify-center font-medium'>
+       <Pressable onPress={() => router.back()}>
+                <Ionicons 
+                  name="arrow-back" 
+                  size={24} 
+                  className="text-gray-900 dark:text-white"
+                />
+              </Pressable> 
+        <Text>
+          This page isn't found {'/n'}
+        </Text>
+    </SafeAreaView>
+  )
 }
